899470017aaadf490c865706b98dbc6e22f4bc74
Nodir <nodirhikmatov6gmail.com>
2025-06-07 04:06:26 +0900

    feat: here is our booking

---
+++ libs/components/layout/LayoutFull.tsx
@@ -0,0 +1,82 @@
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import React, { useEffect } from 'react';
+import { getJwtToken, updateUserInfo } from '../../auth';
+
+import Chat from '../Chat';
+import Footer from '../Footer';
+import Head from 'next/head';
+import { Stack } from '@mui/material';
+import Top from '../Top';
+import useDeviceDetect from '../../hooks/useDeviceDetect';
+import { useReactiveVar } from '@apollo/client';
+import { useRouter } from 'next/router';
+import { userVar } from '../../../apollo/store';
+
+const withLayoutFull = (Component: any) => {
+	return (props: any) => {
+		const router = useRouter();
+		const device = useDeviceDetect();
+		const user = useReactiveVar(userVar);
+
+		/** LIFECYCLES **/
+		useEffect(() => {
+			const jwt = getJwtToken();
+			if (jwt) updateUserInfo(jwt);
+		}, []);
+
+		/** HANDLERS **/
+
+		if (device == 'mobile') {
+			return (
+				<>
+					<Head>
+						<title>Nestar</title>
+						<meta name={'title'} content={`Nestar`} />
+					</Head>
+					<Stack id="mobile-wrap">
+						<Stack id={'top'}>
+							<Top />
+						</Stack>
+
+						<Stack id={'main'}>
+							<Component {...props} />
+						</Stack>
+
+						<Stack id={'footer'}>
+							<Footer />
+						</Stack>
+					</Stack>
+				</>
+			);
+		} else {
+			return (
+				<>
+					<Head>
+						<title>Nestar</title>
+						<meta name={'title'} content={`Nestar`} />
+					</Head>
+					<Stack id="pc-wrap">
+						<Stack id={'top'}>
+							<Top />
+						</Stack>
+
+						<Stack id={'main'}>
+							<Component {...props} />
+						</Stack>
+
+						{user?._id && <Chat />}
+
+						<Stack id={'footer'}>
+							<Footer />
+						</Stack>
+					</Stack>
+				</>
+			);
+		}
+	};
+};
+
+export default withLayoutFull;
---
+++ libs/components/layout/LayoutHome.tsx
@@ -0,0 +1,89 @@
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import React, { useEffect } from 'react';
+import { getJwtToken, updateUserInfo } from '../../auth';
+
+import Chat from '../Chat';
+import FiberContainer from '../common/FiberContainer';
+import Footer from '../Footer';
+import Head from 'next/head';
+import HeaderFilter from '../homepage/HeaderFilter';
+import { Stack } from '@mui/material';
+import Top from '../Top';
+import useDeviceDetect from '../../hooks/useDeviceDetect';
+import { useReactiveVar } from '@apollo/client';
+import { userVar } from '../../../apollo/store';
+
+const withLayoutMain = (Component: any) => {
+	return (props: any) => {
+		const device = useDeviceDetect();
+		const user = useReactiveVar(userVar);
+
+		/** LIFECYCLES **/
+		useEffect(() => {
+			const jwt = getJwtToken();
+			if (jwt) updateUserInfo(jwt);
+		}, []);
+
+		/** HANDLERS **/
+
+		if (device == 'mobile') {
+			return (
+				<>
+					<Head>
+						<title>Nestar</title>
+						<meta name={'title'} content={`Nestar`} />
+					</Head>
+					<Stack id="mobile-wrap">
+						<Stack id={'top'}>
+							<Top />
+						</Stack>
+
+						<Stack id={'main'}>
+							<Component {...props} />
+						</Stack>
+
+						<Stack id={'footer'}>
+							<Footer />
+						</Stack>
+					</Stack>
+				</>
+			);
+		} else {
+			return (
+				<>
+					<Head>
+						<title>Nestar</title>
+						<meta name={'title'} content={`Nestar`} />
+					</Head>
+					<Stack id="pc-wrap">
+						<Stack id={'top'}>
+							<Top />
+						</Stack>
+
+						<Stack className={'header-main'}>
+							{/* <FiberContainer /> */}
+							<Stack className={'container'}>
+								<HeaderFilter />
+							</Stack>
+						</Stack>
+
+						<Stack id={'main'}>
+							<Component {...props} />
+						</Stack>
+
+						{user?._id && <Chat />}
+
+						<Stack id={'footer'}>
+							<Footer />
+						</Stack>
+					</Stack>
+				</>
+			);
+		}
+	};
+};
+
+export default withLayoutMain;
