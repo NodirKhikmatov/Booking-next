ed719f0345ea116dc907a63f7b653fb2acaa879e
Nodir <nodirhikmatov6gmail.com>
2025-06-12 16:57:44 +0900

    feat: grapql error handling for client application

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,11 +1,14 @@
-import { useMemo } from 'react';
-import { ApolloClient, ApolloLink, InMemoryCache, split, from, NormalizedCacheObject } from '@apollo/client';
-import createUploadLink from 'apollo-upload-client/public/createUploadLink.js';
+import { ApolloClient, ApolloLink, InMemoryCache, NormalizedCacheObject, from, split } from '@apollo/client';
+
+import { TokenRefreshLink } from 'apollo-link-token-refresh';
 import { WebSocketLink } from '@apollo/client/link/ws';
+import createUploadLink from 'apollo-upload-client/public/createUploadLink.js';
+import { getJwtToken } from '../libs/auth';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { onError } from '@apollo/client/link/error';
-import { getJwtToken } from '../libs/auth';
-import { TokenRefreshLink } from 'apollo-link-token-refresh';
+import { useMemo } from 'react';
+import { sweetErrorAlert } from '../libs/sweetAlert';
+import { Message } from '@mui/icons-material';
 let apolloClient: ApolloClient<NormalizedCacheObject>;
 
 function getHeaders() {
@@ -61,6 +64,9 @@ function createIsomorphicLink() {
 			if (graphQLErrors) {
 				graphQLErrors.map(({ message, locations, path, extensions }) =>
 					console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
+				
+				if(!Message.includes("input")) sweetErrorAlert(message)
+				
 				);
 			}
 			if (networkError) console.log(`[Network error]: ${networkError}`);
