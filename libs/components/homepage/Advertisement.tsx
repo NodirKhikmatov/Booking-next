77acf5c3ebbdee805c99733fba515cd0eb3a28d8
Nodir <nodirhikmatov6gmail.com>
2025-06-14 05:46:44 +0900

    feat: develop advertisemnt page

--- libs/components/homepage/Advertisement.tsx
+++ libs/components/homepage/Advertisement.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import useDeviceDetect from '../../hooks/useDeviceDetect';
 import { Stack } from '@mui/material';
+import useDeviceDetect from '../../hooks/useDeviceDetect';
 
 const Advertisement = () => {
 	const device = useDeviceDetect();
@@ -16,7 +16,7 @@ const Advertisement = () => {
 					preload="auto"
 					style={{ width: '100%', height: '100%', objectFit: 'cover' }}
 				>
-					<source src="/video/ads.mov" type="video/mp4" />
+					<source src="/video/1.mp4" type="video/mp4" />
 				</video>
 			</Stack>
 		);
@@ -31,7 +31,7 @@ const Advertisement = () => {
 					preload="auto"
 					style={{ width: '100%', height: '100%', objectFit: 'cover' }}
 				>
-					<source src="/video/ads.mov" type="video/mp4" />
+					<source src="/video/1.mp4" type="video/mp4" />
 				</video>
 			</Stack>
 		);
